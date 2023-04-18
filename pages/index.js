7522dc25a6414305fb13ed1a8ae37938592a2e85
Abderrahmane <tiamani3939@gmail.com>
2023-04-18 12:27:53 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Sidebar from "@/components/Sidebar";
+import PostFeed from "@/components/PostFeed";
+import Trending from "@/components/Trending";
+import BottomBanner from "@/components/BottomBanner";
+import CommentModal from "@/components/modals/CommentModal";
+
+import { useSelector } from "react-redux";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const username = useSelector((state) => state.user.username);
+  return (
+    <div className=" bg-[#242424]  min-h-screen">
+      <Navbar/>
+      <div className=" flex bg-[#242424] text-[#E7E9EA] max-w-[1400px] mx-auto ">
+        <Sidebar />
+        <PostFeed />
+        <Trending />
+      </div>
+      <CommentModal/>
+      {!username && <BottomBanner />}
+    </div>
+  );
+}
